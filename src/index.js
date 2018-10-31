22d25ada0577af32c77faee4b458b6d1f4da7a16
Esfahani <esfahanimichael@gmail.com>
2018-10-30 22:22:46 -0700

    finished proto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const user = {
+    name : 'ryan',
+    luckyNumber: luckyNumber()
+
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+
+function greeting(user){
+    return <div className ="container">
+        <h1>Hello to you mister {user.name}</h1>
+        <h2 className ='text-muted'> This is the lucky number: {user.luckyNumber}</h2>
+    </div>;
+
+}
+
+
+
+
+
+
