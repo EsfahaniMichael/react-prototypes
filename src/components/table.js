1549de026d0ed8cfdd8dfe2440de8da9d15ff31c
Esfahani <esfahanimichael@gmail.com>
2018-11-01 22:04:46 -0700

    updated

---
+++ src/components/table.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+const CreatingTable = (props)=> {
+const tableRows = props.data.map((item, index) => {
+    console.log(props)
+    return (<tr key={index}>
+        <td>{item.name}</td>
+        <td>{item.course}</td>
+        <td>{item.grade}</td>
+    </tr>);
+
+
+
+});
+
+
+  return (      <table className="table">
+            <thead className="thead-inverse">
+            <tr>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Grade</th>
+            </tr>
+            </thead>
+            <tbody>
+            {tableRows}
+
+            </tbody>
+        </table>
+
+    )
+}
+
+export default CreatingTable;
