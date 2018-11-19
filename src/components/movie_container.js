5634fa31401a824fbc6cb81c7729d3790539c725
Esfahani <esfahanimichael@gmail.com>
2018-11-19 11:33:42 -0800

    finished prototype

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import Movie from './movies';
+import axios from 'axios';
+
 
 class MoviesContainer extends Component{
+constructor(props){
+    super(props);
+
+    this.state = {
+        movies: []
+    }
+}
+
+    componentWillMount(){
+        console.log('hi');
+        const URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+
+        axios.get(URL).then((resp) => {
+            console.log('resp', resp);
+            this.setState({
+                movies: resp.data.feed.entry
+            })
+        });
+
+}
     render(){
+    console.log('stateeee', this.state)
+
+       const movieList = this.state.movies.map((movieInfo, index) => {
+            return (
+                <Movie info={movieInfo} key={index}/>
+            )
+        });
+
         return(
             <div>
-                <h2>
-                    Movie Container
-                </h2>
-                <Movie/>
+                {movieList}
             </div>
         )
 
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export default props => {
+    console.log('movieee info', props.info)
+    const { info } = props;
     return (
+
+
         <div>
-            <h3>
-                Movie
-            </h3>
+            <img src={info['im:image'][2].label}/>
+            <h3>{info['im:name'].label}</h3>
+            <p>{info['summary'].label}</p>
         </div>
+
     )
 }
