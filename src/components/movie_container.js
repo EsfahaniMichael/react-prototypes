f6142898e5e110acda802fb636343b1e3f71d1a5
Esfahani <esfahanimichael@gmail.com>
2018-11-18 20:33:00 -0800

    dinner

---
+++ src/components/movie_container.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Movie from './movies';
+
+class MoviesContainer extends Component{
+    render(){
+        return(
+            <div>
+                <h2>
+                    Movie Container
+                </h2>
+                <Movie/>
+            </div>
+        )
+
+    }
+}
+
+export default MoviesContainer;
---
+++ src/components/movies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => {
+    return (
+        <div>
+            <h3>
+                Movie
+            </h3>
+        </div>
+    )
+}
