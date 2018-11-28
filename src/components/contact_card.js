2df0e0023a5be306c24b4815cc9154c58fd6595a
Esfahani <esfahanimichael@gmail.com>
2018-11-27 20:23:05 -0800

    updates

---
+++ src/components/contact_card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default props => {
+    return (
+      <div className="col-6 my-3">
+          <div className="card">
+              <div className="card-header"></div>
+              <div className="card-block">
+                  <h4 className="card-title"></h4>
+                  <div className="card-text">
+                      <p><b>Email:</b></p>
+                      <p><b>Phone:</b></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+    )
+}
