853e7196fdd8847a73bb810525bbb887553f7f12
Esfahani <esfahanimichael@gmail.com>
2018-11-27 17:48:52 -0800

    updates

---
+++ src/components/scrapbook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './scrapbook.css'
+import Toad from './images/Toad.jpg';
+
+
+export default props => {
+   return (
+       <div className="scrapbook-container">
+
+       </div>
+   )
+}
